0b8f376dd44b97ec270427895e62faa23b064447
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2025-09-04 20:14:05 +0000

    feat: adding initial hero section in landing page

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -1,44 +1,67 @@
+import PublicHeader from "@/components/public-pages/header";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <header className="flex w-[100%] justify-between pt-4 pl-8 pr-8">
-        <div className="w-[25%]">
-
-          <Image 
-            src="/images/flux-logo.png"
-            alt="flux-logo"
-            width={100}
+      <PublicHeader />
+      <div className="relative">
+        <div className="flex w-[100%] pt-4 pl-8 pr-8">
+          <Image
+            src="/images/light-hero-1.png"
+            alt="light"
+            width={1000}
             height={100}
             style={{
               height: 'auto'
             }}
+            className="absolute rounded-tl-[3rem]"
           />
+          <Image
+            src="/images/light-hero-2.png"
+            alt="light"
+            width={1000}
+            height={100}
+            style={{
+              height: 'auto'
+            }}
+            className="absolute rounded-tr-[3rem] right-8"
+          />
+          <Image
+            src="/images/details-with-mask.png"
+            alt="light"
+            width={600}
+            height={100}
+            style={{
+              height: 'auto'
+            }}
+            className="absolute rounded-tr-[3rem] left-[35%] top-45"
+          />
+
+          <div className="absolute left-[42%] top-85 text-center">
+            Get Started with
+            <br />
+            <Image
+              src="/images/flux-logo.png"
+              alt="light"
+              width={250}
+              height={100}
+              style={{
+                height: 'auto'
+              }}
+            />
+          </div>
+
+          <div className="flex w-[100%] border-1 border-(--border-muted) h-[90dvh] bg-(--foreground-color) rounded-[3rem] p-12">
+            
+            <div className="flex self-end">
+              <button className="border-1 border-(--border-muted) pt-5 pb-5 pl-10 pr-10 rounded-[3rem] hover:bg-(--cp) hover:text-(--foreground-color) font-semibold transition duration-150 ease-in-out hover:border-(--cp) hover:translate-x-1 hover:translate-y-[-4px]">
+                Get Started
+              </button>
+            </div>
+          </div>
         </div>
-        <nav className="flex w-[50%] justify-center items-center">
-          <ul className="flex w-full justify-center gap-12 text-(--muted)  font-(font-family: var(--font-inter) font-normal">
-            <li className="hover:text-(--text-light) transition duration-200 ease-in-out">
-              <a href="#about">About</a>
-
-            </li>
-            <li className="hover:text-(--text-light) transition duration-200 ease-in-out">
-              <a href="#pricing">Pricing</a>
-
-            </li>
-          </ul>
-
-        </nav>
-        <div className="w-[25%]">
-
-        </div>
-        <button className="w-[120px] bg-(--cp) rounded-lg text-(--foreground-color) font-semibold p-2 hover:brightness-85 transition duration-300 ease-in-out active:scale-95">
-          <Link href="signup">
-            Sign Up
-          </Link>
-        </button>
-      </header>
+      </div>
     </main>
   );
 }
---
+++ frontend/src/components/public-pages/header.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+
+
+export default function PublicHeader () {
+    
+    return (
+        <>
+            <header className="flex w-[100%] justify-between pt-4 pl-8 pr-8">
+                <div className="w-[25%]">
+
+                <Image 
+                    src="/images/flux-logo.png"
+                    alt="flux-logo"
+                    width={100}
+                    height={100}
+                    style={{
+                    height: 'auto'
+                    }}
+                />
+                </div>
+                <nav className="flex w-[60%] justify-center items-center">
+                <ul className="flex w-full justify-center gap-12 text-(--muted)  font-(font-family: var(--font-inter) font-normal">
+                    <li className="hover:text-(--text-light) transition duration-200 ease-in-out">
+                    <a href="#about">About</a>
+
+                    </li>
+                    <li className="hover:text-(--text-light) transition duration-200 ease-in-out">
+                    <a href="#pricing">Pricing</a>
+
+                    </li>
+                </ul>
+
+                </nav>
+                <div className="w-[25%]">
+
+                </div>
+                <button className="w-[100px] bg-(--cp) rounded-xl text-(--foreground-color) border-1 text-sm font-semibold pb-3 pt-3 pl-2 pr-2 hover:brightness-85 transition duration-300 ease-in-out active:scale-95">
+                <Link href="signup">
+                    Sign Up
+                </Link>
+                </button>
+            </header>
+        </>
+    )
+}
